84261938389ceb07f74832d9f1157a05af699d84
Kateryna <keitkat98@gmail.com>
2024-05-20 17:20:06 +0300

    added book model and book service

---
+++ src/main/java/mate/academy/bookapp/model/Book.java
@@ -0,0 +1,26 @@
+package mate.academy.bookapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    @Column(name = "cover_image")
+    private String coverImage;
+}
