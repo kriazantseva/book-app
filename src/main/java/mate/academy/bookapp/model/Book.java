7946c003d05a3dccaa676ddcb4ea81dabb7cd82a
Kateryna <keitkat98@gmail.com>
2024-05-21 12:54:35 +0300

    i think i finally understood

--- src/main/java/mate/academy/bookapp/model/Book.java
+++ src/main/java/mate/academy/bookapp/model/Book.java
@@ -7,33 +7,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "books")
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
 @ToString(exclude = {"description", "coverImage"})
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Long id;
     @Column(nullable = false)
-    @EqualsAndHashCode.Include
     private String title;
     @Column(nullable = false)
-    @EqualsAndHashCode.Include
     private String author;
     @Column(nullable = false, unique = true)
-    @EqualsAndHashCode.Include
     private String isbn;
     @Column(nullable = false)
-    @EqualsAndHashCode.Include
     private BigDecimal price;
     private String description;
     @Column(name = "cover_image")
